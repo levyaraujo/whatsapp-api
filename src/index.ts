d2c506d35aa02350d91ad2a7b98c8b48e5bc3deb
Levy Araujo <lev0x79@gmail.com>
2023-02-11 14:00:47 -0300

    Add post route

--- src/index.ts
+++ src/index.ts
@@ -9,5 +9,5 @@ const port = process.env.PORT || 8080;
 
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on port ${port} 🚀`);
 });
