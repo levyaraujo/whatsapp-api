665253be2b072019818501f4f48959fc6cfff0b3
Levy Araujo <lev0x79@gmail.com>
2023-02-27 21:15:12 -0300

    Fix

--- src/controller/messageListener.ts
+++ src/controller/messageListener.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { Bot } from "../utils/messageHandler";
 
 export async function messageListener(request: Request, response: Response) {
-  const data = request.body.body;
+  const data = request.body;
   console.log(data);
   const bot = new Bot("Levy", "5594981362600");
   await bot.onboard();
